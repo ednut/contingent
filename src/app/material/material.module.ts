1f096e1087b273ba12cbc29c792c2263ea96f808
ednut <babatundeadegbite@gmail.com>
2022-01-23 17:59:15 +0000

    making adjustmests to markup

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatCardModule} from '@angular/material/card';
 
 
 const MaterialComponents = [
   MatGridListModule,
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatChipsModule,
+  MatCardModule
 ]
 
 @NgModule({
