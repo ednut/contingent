ef9deca99ee3039cd3dbf1926e236da8338f02f9
ednut <babatundeadegbite@gmail.com>
2022-01-23 14:02:30 +0000

    working on details page

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 const MaterialComponents = [
   MatGridListModule,
-  MatButtonModule
+  MatButtonModule,
+  MatIconModule
 ]
 
 @NgModule({
